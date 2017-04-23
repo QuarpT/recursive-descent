653da29ae67fdad21f7fe13a6821c01c12aa9ffe
Peter C <zvvvvt@gmail.com>
2017-04-23 18:50:33 +0100

    hlist type safe

--- src/main/scala/parse/parser.scala
+++ src/main/scala/parse/parser.scala
@@ -1,36 +1,21 @@
 package parse
 import parse.Rule.ImplicitCat
+import shapeless.ops.hlist.Prepend
+import shapeless.{::, HList, HNil}
 
 sealed trait Token
 
-sealed trait RuleOutput {
-  def merge(ruleOutput: RuleOutput): RuleOutput
+case class Matched[A <: HList](parsed: A, remaining: Seq[Token])
+case class RuleOutput[A <: HList](matches: Seq[Matched[A]]) {
+  def merge(ruleOutput: RuleOutput[A]): RuleOutput[A] = RuleOutput(matches ++ ruleOutput.matches)
 }
-case object Unmatched extends RuleOutput {
-  override def merge(ruleOutput: RuleOutput): RuleOutput = ruleOutput
-}
-case class Matched(parsed: Seq[Tree], remaining: Seq[Token])
-case class Matches(matches: Seq[Matched]) extends RuleOutput {
-  def ruleOutput: RuleOutput = matches.headOption.fold[RuleOutput](Unmatched)(_ => this)
-  override def merge(ruleOutput: RuleOutput): RuleOutput = ruleOutput match {
-    case ms: Matches => Matches(ms.matches ++ matches)
-    case Unmatched => this
-  }
-}
-object RuleOutput {
-  def merge(output: Seq[RuleOutput]): RuleOutput = Matches(output.foldLeft(Seq.empty[Matched]) {
-    case (accum, ms: Matches) => ms.matches ++ ms.matches
-    case (accum, _) => accum
-  }).ruleOutput
-}
-
 
 sealed trait Tree
 
 object Rule {
-  implicit class ImplicitCat(rule: Rule) {
-    def *(concatMe: => Rule): ImplicitCat = new RuleConcat(rule, concatMe)
-    def >(fun: Seq[Tree] => Seq[Tree]): Rule = CompletingRule(rule, fun)
+  implicit class ImplicitCat[A](rule: Rule[A]) {
+    def *[B](concatMe: => Rule[B]): ImplicitCat[Prepend[A, B]#Out] = new RuleConcat(rule, concatMe)
+    def >[B](fun: A => B): Rule = CompletingRule(rule, fun)
   }
 
   implicit class ImplicitOr(rule: Rule) {
@@ -38,10 +23,13 @@ object Rule {
   }
 }
 
-case class CompletingRule(rule: Rule, fun: Seq[Tree] => Seq[Tree]) extends Rule {
+case class CompletingRule[A <: HList, B <: HList](rule: Rule[A], fun: A => B) extends Rule {
   override def parse(remaining: Matched): RuleOutput = {
-    rule.parse(remaining) match {
-      case Matches(ms) => Matches(ms.map(m => Matched(fun(m.parsed), m.remaining)))
+    val matches = rule.parse(remaining)
+
+    RuleOutput(matches.matches.map(m => Matched(fun(m.parsed), m.remaining)))
+    match {
+      case Matches(ms) =>
       case Unmatched => Unmatched
     }
   }
@@ -51,30 +39,26 @@ case class RuleOr(ruleLeft: Rule, ruleRight: Rule) extends Rule {
   override def parse(remaining: Matched): RuleOutput = ruleLeft.parse(remaining) merge ruleRight.parse(remaining)
 }
 
-class RuleConcat(ruleLeft: Rule, ruleRight: => Rule) extends Rule {
-  override def parse(remaining: Matched): RuleOutput = {
-    ruleLeft.parse(remaining) match {
-      case Matches(matches) => applyRightRuleToMatches(matches)
-      case Unmatched => Unmatched
-    }
-  }
+class RuleConcat[A <: HList, B <: HList](ruleLeft: Rule[A], ruleRight: => Rule[B])(implicit prepend : Prepend[A, B]) extends Rule[Prepend[A, B]#Out] {
+  type PrependType = Prepend[A, B]#Out
+  type MatchedType = Matched[PrependType]
 
-  private def applyRightRuleToMatches(matches: Seq[Matched]): RuleOutput = {
-    val ruleOutput = Matches(matches.foldLeft(Seq.empty[Matched]) { (accum, matched) =>
-      ruleRight.parse(matched) match {
-        case Matches(a) => a ++ accum
-        case Unmatched => accum
+  override def parse[C <: HList](remaining: Matched[C]): RuleOutput[PrependType] = {
+    val a: RuleOutput[Prepend[C, A]#Out] = ruleLeft.parse(remaining)
+    val matches: Seq[MatchedType] = a.matches.foldLeft(Seq.empty[MatchedType]) { (accum, leftMatch) =>
+      val rightMatches: Seq[Matched[B]] = ruleRight.parse(leftMatch.remaining).matches
+      val mergeMatches = rightMatches.foldLeft(Seq.empty[MatchedType]) { (accum: Seq[MatchedType], rightMatch) =>
+        Matched(leftMatch.parsed ++ rightMatch.parsed, rightMatch.remaining) +: accum
       }
-    })
-    ruleOutput.ruleOutput
+      accum ++ mergeMatches
+    }
+    RuleOutput(matches)
   }
 }
 
-sealed trait Rule {
-  def parse(remaining: Matched): RuleOutput
-  def parse(tokens: Seq[Token]): RuleOutput = {
-    parse(Matched(Seq.empty, tokens))
-  }
+sealed trait Rule[A <: HList] {
+  def parse[B <: HList](remaining: Matched[B]): RuleOutput[Prepend[B, A]#Out]
+  def parse(tokens: Seq[Token]): RuleOutput[Prepend[HNil, A]#Out] = parse(Matched[HNil](HNil, tokens))
 }
 
 object Arithmetic {
@@ -86,10 +70,9 @@ object Arithmetic {
   case class Operator(op: Char) extends Tree
   case class Num(num: Int) extends Expression
 
-  object NumRule extends Rule {
-    override def parse(matched: Matched): RuleOutput = matched.remaining match {
-      case NumToken(x) :: tail => Matches(Matched(matched.parsed :+ Num(x), tail) :: Nil)
-      case _ => Unmatched
+  object NumRule extends Rule[::[Num, HNil]] {
+    override def parse[B <: HList](remaining: Matched[B]): RuleOutput[Prepend[B, ::[Num, HNil]]#Out] = {
+      remaining.parsed
     }
   }
 
