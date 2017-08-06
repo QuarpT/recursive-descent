4b91b759beeb92603addd6a741ee43a2c67b5456
Peter C <zvvvvt@gmail.com>
2017-08-06 18:15:20 +0100

    remove main

--- src/main/scala/org/peterc/srdp/Main.scala
+++ src/main/scala/org/peterc/srdp/Main.scala
@@ -1,12 +1,6 @@
 package org.peterc.srdp
-import org.peterc.srdp.examples.arithmetic.Arithmetic._
-import org.peterc.srdp.examples.json.Json
-/**
-  * Created by peterc on 28/06/2017.
-  */
+
 object Main {
   def main(str: Array[String]) = {
-    println(eval"1+2")
-    println(eval"((100 + 300) / (4 * 25)) - 2")
   }
 }
